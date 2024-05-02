e0bee5512bdfc0b56c4d3992b22bd3a06d67df97
victor64x4 <rm24004@ues.edu.sv>
2024-05-02 16:42:36 -0600

    Archivos Nuevos

---
+++ demo.RM24004/src/main/java/com/iss/RM24004/demo/RM24004/App.java
@@ -0,0 +1,18 @@
+package com.iss.RM24004.demo.RM24004;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String Hello()
+    {
+        return "Hello World!";
+    }
+    public static void main (String [] args)
+    {
+    	App hw = new App();
+    	System.out.println(hw.Hello());
+    }
+}
---
+++ demo.RM24004/src/test/java/com/iss/RM24004/demo/RM24004/AppTest.java
@@ -0,0 +1,20 @@
+package com.iss.RM24004.demo.RM24004;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
